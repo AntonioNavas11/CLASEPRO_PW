6c26f5d0025e9471218971e51e1a4b1b1fcd6696
AntonioNavas11 <antonio92.anm@gmail.com>
2025-10-06 02:50:37 +0200

    Update
    Ultimos cambios clase 2 programacion

--- ProyectoInicial/src/Main.java
+++ ProyectoInicial/src/Main.java
@@ -1,2 +1,23 @@
 public class Main {
+
+    // Esto es un comentario
+    /*
+    Este comentario
+    se puede dividir
+    en varias lineas de codigo
+     */
+
+    // TODO me falta esto
+
+    public static void main (String[]arg){
+        System.out.print("Este es mi primer programa en Java\n");
+        System.out.print("Este es la segunda linea del programa\n");
+
+    }
+
+
+    public void nombreMetodo (){
+
+
+    }
 }
